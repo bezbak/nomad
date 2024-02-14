b7ca9a017ca528895ce2ea0ee46e257073ccaec7
Baiaz Orozbekov <baiaz.orozbekov@gmai.com>
2024-02-14 15:39:23 +0600

    added languages

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import ToursDetails from "./components/tours-details/ToursDetails.jsx";
 import Card from "./components/cards/Card.jsx";
 import Navbar from './components/navbar/Navbar.jsx';
 import Footer from './bot/Footer.jsx'
-import AboutUs from './bot/AboutUs.jsx'
-import Reviews from './components/Reviews/Reviews.jsx'
-import Numbers from './bot/Numbers.jsx'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
 function App() {
+
   return (
     <>  
       <Navbar/>
--- src/features/Filters/Filters-country.jsx
+++ src/features/Filters/Filters-country.jsx
@@ -1,9 +1,11 @@
 import {setFilter} from "./Filter-slice.js";
 import {useDispatch, useSelector} from "react-redux";
 import '../features.css'
+import {useTranslation} from "react-i18next";
 
 const FiltersCountry = () => {
 
+    const {t} = useTranslation()
     const dispatch = useDispatch()
     const activeFilter = useSelector(state => state.filterReducer)
 
@@ -11,13 +13,13 @@ const FiltersCountry = () => {
         <div>
             <div className="buttons">
                 <button className={ activeFilter === 'kg' ? 'countryes countryes-active' : 'countryes '}
-                        onClick={() => dispatch(setFilter('kg'))}>Kyrgyzstan
+                        onClick={() => dispatch(setFilter('kg'))}> {t('kgz')}
                 </button>
                 <button className={ activeFilter === 'kz' ? 'countryes countryes-active' : 'countryes '}
-                        onClick={() => dispatch(setFilter('kz'))}>Kazakhstan
+                        onClick={() => dispatch(setFilter('kz'))}> {t('kaz')}
                 </button>
                 <button className={ activeFilter === 'uz' ? 'countryes countryes-active' : 'countryes '}
-                        onClick={() => dispatch(setFilter('uz'))}>Uzbekistan
+                        onClick={() => dispatch(setFilter('uz'))}> {t('uzb')}
                 </button>
             </div>
         </div>
